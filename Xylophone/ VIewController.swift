23617c5d9d7bdbaaaae72031c220a864921a05b8
Çağrı Aydın <c.aydinakdogan@gmail.com>
2018-08-20 15:57:25 +0300

    Initial commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Çağrı Aydın on 20.08.2018.
+//  Copyright © 2018 Çağrı Aydın. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+
+    
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+    
+    }
+}
+

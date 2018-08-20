b3454b9a51b1aea1c5242c3c37ac81fd38664cb6
Çağrı Aydın <c.aydinakdogan@gmail.com>
2018-08-20 16:40:18 +0300

    pressed event implemented

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -10,16 +10,23 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController{
-
     
-    var player: AVAudioPlayer?
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-    
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch{
+            print(error)
+        }
+        audioPlayer.play()
     }
 }
 
